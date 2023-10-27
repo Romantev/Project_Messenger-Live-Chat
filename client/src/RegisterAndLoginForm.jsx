388673e948ea986df4bcc45602546d2dc2191f07
Roman Parfentev <romanntev@gmail.com>
2023-10-27 13:11:39 +0200

    fixed some bugs

--- client/src/RegisterAndLoginForm.jsx
+++ client/src/RegisterAndLoginForm.jsx
@@ -1,19 +1,37 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "./UserContext.jsx";
 import axios from "axios";
 
 export default function RegisterAndLoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, seterrorMessage] = useState(false);
   const [isLoginOrRegister, setIsLoginOrRegister] = useState("register");
   const { setUsername: setLoggedInUsername, setId } = useContext(UserContext);
 
+  useEffect(() => {
+    if (password.length > 0 || confirmPassword > 0) {
+      seterrorMessage(false);
+    }
+  }, [password]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const url = isLoginOrRegister === "register" ? "register" : "login";
-    const { data } = await axios.post(`/api/${url}`, { username, password });
-    setLoggedInUsername(username);
-    setId(data.id);
+
+    if (isLoginOrRegister === "register") {
+      if (password === confirmPassword) {
+        const url = isLoginOrRegister === "register" ? "register" : "login";
+        const { data } = await axios.post(`/api/${url}`, {
+          username,
+          password,
+        });
+        setLoggedInUsername(username);
+        setId(data.id);
+      } else {
+        seterrorMessage(true);
+      }
+    }
   };
 
   return (
@@ -33,23 +51,39 @@ export default function RegisterAndLoginForm() {
           onChange={(e) => setPassword(e.target.value)}
           className="block w-full rounded-sm p-2 mb-2 border"
         />
+        {isLoginOrRegister === "register" && (
+          <input
+            type="password"
+            placeholder="confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="block w-full rounded-sm p-2 mb-2 border"
+          />
+        )}
         <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
           {isLoginOrRegister === "register" ? "Register" : "Login"}
         </button>
         <div className="text-center mt-2">
           {isLoginOrRegister === "register" && (
-            <div>
+            <div className="flex flex-col">
               Already a member?
-              <button onClick={() => setIsLoginOrRegister("login")}>
+              <button
+                onClick={() => setIsLoginOrRegister("login")}
+                className="text-blue-500">
                 Login here
               </button>
             </div>
           )}
+          {errorMessage && (
+            <h3 className="text-red-500">Password combination invalid</h3>
+          )}
           {isLoginOrRegister === "login" && (
-            <div>
+            <div className="flex flex-col">
               Don't have an account?
-              <button onClick={() => setIsLoginOrRegister("register")}>
-                Login here
+              <button
+                onClick={() => setIsLoginOrRegister("register")}
+                className="text-blue-500">
+                Register here
               </button>
             </div>
           )}
